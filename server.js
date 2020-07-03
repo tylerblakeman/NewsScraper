70c8caf53798f50941a1503c2ff695b49c3be076
Tyler Blakeman <tylerblakeman@gmail.com>
2020-07-03 09:52:29 -0500

    server is live with html and api routes

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+//setting port for environment or port 3000 - locally will run on port 3000
+var PORT = 3004;
+
+// Require all models
+// var db = require("./models");
+
+//initializing express
+var app = express();
+
+//requiring the routes folder
+require('./routes/apiRoutes.js')(app)
+require('./routes/htmlRoutes.js')(app)
+
+//setting view engine to express-handlebars
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Make public a static folder
+app.use(express.static("public"));
+
+// Start the server
+app.listen(PORT, function() {
+  console.log(`App running on port${PORT}!`);
+});
