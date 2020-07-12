302a76e285597a78e9ab08d15d31a0619d2a0625
Tyler Blakeman <tylerblakeman@gmail.com>
2020-07-11 20:59:34 -0500

    removed note model

--- models/note.js
+++ models/note.js
@@ -1,17 +1,17 @@
-var mongoose = require("mongoose");
+// var mongoose = require("mongoose");
 
 
-var Schema = mongoose.Schema;
+// var Schema = mongoose.Schema;
 
-var NoteSchema = new Schema({
-  // `title` is of type String
-  title: String,
-  // `body` is of type String
-  body: String
-});
+// var NoteSchema = new Schema({
+//   // `title` is of type String
+//   title: String,
+//   // `body` is of type String
+//   body: String
+// });
 
 
-var Note = mongoose.model("Note", NoteSchema);
+// var Note = mongoose.model("Note", NoteSchema);
 
-// Export the Note model
-module.exports = Note;
+// // Export the Note model
+// module.exports = Note;
